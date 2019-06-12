a4dcfda88e74f6d4fdd240e65652ad13c24dba5a
Dennis Wanjiru <denniswanjiru71@gmail.com>
2019-06-12 21:43:47 +0300

    Write tests for App and Row components

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
+import { shallow } from 'enzyme';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Row from './Row'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders  <App /> components', () => {
+    const snap = shallow(<App />);
+    expect(snap).toMatchSnapshot();
+  });
+});
+
+describe('<Row />', () => {
+  it('renders  <Row /> components', () => {
+    const snap = shallow(<Row className data={{ index: "Index", phone: 'Phone Numbers' }} onClick={() => {}} />);
+    expect(snap).toMatchSnapshot();
+  });
 });
